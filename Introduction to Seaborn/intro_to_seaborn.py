5c2c0fe35350fa208cbfcde731cc3469b769dd42
Arpit <arpitran@buffalo.edu>
2020-04-18 21:21:25 -0400

    Intro to Seaborn

---
+++ Introduction to Seaborn/intro_to_seaborn.py
@@ -0,0 +1,17 @@
+
+import seaborn as sns
+import pandas as pd
+df = pd.read_csv('data/survey.txt')
+display(df)
+
+import math
+
+print("Hello World")
+
+x = int(input("Enter a number\n"))
+y = int(input("Enter another numebr\n"))
+
+hypo = x**2 + 11**2
+h = math.sqrt(hypo)
+
+print(h)
